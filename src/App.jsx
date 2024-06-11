5b31d955cabd76a827a840c433fd2f47bf006928
Gustavo Lozada <gustavo@huperoon.net>
2024-06-11 18:36:43 -0500

    exam upload

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <section id="events">
+        <div className="cs-container">
+          <div className="cs-events">
+            <span className="cs-topper">Our Events</span>
+            <h2 className="cs-title">Upcoming Events</h2>
+          </div>
+        </div>        
+      </section>
+    </>
+  )
+}
+
+export default App
